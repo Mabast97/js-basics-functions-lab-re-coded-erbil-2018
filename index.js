82b055e3bcfa0278ddafaf983f32253d9e0c47bc
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-07-28 17:20:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function calculatesFarePrice (start, destination) {
   if (distance <= 400) {
     return 0;
   } else if (distance > 400 && distance <= 2000) {
-    return .05 * distance;
+    return 1.05 * distance;
   } else if (distance > 2000 && distance < 2500) {
     return 25;
   } else {
